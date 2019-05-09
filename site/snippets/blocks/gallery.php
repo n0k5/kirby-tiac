82b081668af82f1247e20b70461a6ce46937dd28
n0k5 <hey@chrish.design>
2019-05-09 19:56:13 +0100

    Further work on project pages

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -4,13 +4,20 @@
     <!-- should allow me to hotswap images depending on device size -->
     <!-- https://getkirby.com/docs/guide/templates/resize-images-on-the-fly#responsive-images -->
 
-    <?php foreach($data->gallery() as $items): ?>
+    <?php foreach($data->gallery()->toFiles() as $file): ?>
+        <img src="<?= $file->crop(1280, 694, 'center')->url() ?>" alt="<?= $file->alt() ?>">
+    <?php endforeach; ?>
+
+    <!-- <?php foreach($data->gallery() as $items): ?>
+      <?php //dump($items); ?>
       <?php foreach($items as $item): ?>
 
-          <?php echo page($item)->cover()->crop(1280, 694, 'center'); ?>
+          <?php echo $item; ?>
+
+          <?php //echo page($item)->cover()->crop(1280, 694, 'center'); ?>
 
       <?php endforeach; ?>
-    <?php endforeach; ?>
+    <?php endforeach; ?> -->
   </div>
 
   <p class="flickity-caption"></p>
