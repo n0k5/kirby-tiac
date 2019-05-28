5bf45aa244b826e37804acd93273f76d8dcab179
n0k5 <hey@chrish.design>
2019-05-28 20:04:18 +0100

    Add contained gallery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
-var flkty = new Flickity( '.gallery__slides', {
-  imagesLoaded: true,
-  percentPosition: false
-});
+// Loop through all galleries on the page and add flickity to them
+var fullWidthGalleryElems = document.querySelectorAll('.gallery--slides');
+for(var i = 0, len = fullWidthGalleryElems.length; i < len; i++) {
+  var galleryElem = fullWidthGalleryElems[i];
+  var flkty = new Flickity(galleryElem, {
+    imagesLoaded: true,
+    percentPosition: false
+  });
+}
 
-var caption = document.querySelector('.flickity-caption');
-
-console.log(caption);
-
-flkty.on( 'select', function() {
-  // set image caption using img's alt
-  caption.textContent = flkty.selectedElement.alt;
-});
+// // Add captions
+// var captionElems = document.querySelectorAll('.flickity-caption');
+// for(var i = 0, len = captionElems.length; i < len; i++) {
+//   var captionElem = captionElems[i];
+//   flkty.on('select', function() {
+//     caption.textContent = flkty.selectedElement.alt; // set image caption using img's alt
+//   });
+// }
--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -1,11 +1,7 @@
-<div class="gallery <?= ($data->galleryWidth()->value() == 'true' ? 'gallery--fullWidth' : 'gallery--contained') ?>">
-  <div class="gallery__slides">
-    <!-- todo: use srcset here -->
-    <!-- should allow me to hotswap images depending on device size -->
-    <!-- https://getkirby.com/docs/guide/templates/resize-images-on-the-fly#responsive-images -->
-
+<div class="gallery <?= ($data->galleryWidth()->value() == 'true' ? 'gallery--fullWidth' : 'gallery--contained container') ?>">
+  <div class="gallery--slides">
     <?php foreach($data->gallery()->toFiles() as $file): ?>
-        <img src="<?= $file->crop(1280, 694, 'center')->url() ?>" alt="<?= $file->caption() ?>">
+      <img src="<?= $file->crop(1280, 694, 'center')->url() ?>" alt="<?= $file->caption() ?>">
     <?php endforeach; ?>
   </div>
 
@@ -13,3 +9,10 @@
     <p class="flickity-caption"></p>
   <?php endif; ?>
 </div>
+
+    <!-- todo: use srcset on gallery images -->
+    <!-- should allow me to hotswap images depending on device size -->
+    <!-- https://getkirby.com/docs/guide/templates/resize-images-on-the-fly#responsive-images -->
+
+    <!-- todo: add captions -->
+    <!-- https://github.com/metafizzy/flickity/issues/224 -->
