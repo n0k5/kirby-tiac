6f70521e41274c7c426eebda63e8975a2d471121
n0k5 <hey@chrish.design>
2019-06-03 19:17:54 +0100

    Fix project thumbnail bug

--- site/snippets/blocks/grid.php
+++ site/snippets/blocks/grid.php
@@ -1,9 +1,21 @@
-<div class="grid">
+<div class="imgGrid container">
 
   <?php foreach($data->gridImages()->toFiles() as $file): ?>
-    <div class="grid__image">
-        <img src="<?= $file->url() ?>" alt="<?= $file->alt() ?>">
+    <div class="imgGrid__item">
+
+      <div class="imgGrid__image" style="background-image: url(<?= $file->crop(456, 264, 'center')->url() ?>)">
+        <img src="<?= $file->crop(456, 264, 'center')->url() ?>" alt="<?= $file->alt() ?>">
+      </div>
+
+      <? if($file->caption()->isNotEmpty()): ?>
+        <div class="imgGrid__caption">
+          <p><?= $file->caption() ?></p>
+        </div>
+      <?php endif; ?>
+
     </div>
   <?php endforeach; ?>
 
 </div>
+
+<!-- todo: srcset images on this page -->
