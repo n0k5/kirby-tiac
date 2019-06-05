f58c414b5a8c6293775b1f857bf0abf7c3b9ae3e
n0k5 <hey@chrish.design>
2019-06-05 18:03:50 +0100

    Update image grid

--- site/snippets/blocks/grid.php
+++ site/snippets/blocks/grid.php
@@ -1,21 +1,23 @@
-<div class="imgGrid container">
+<div class="imgGrid">
+  <div class="container">
 
-  <?php foreach($data->gridImages()->toFiles() as $file): ?>
-    <div class="imgGrid__item">
+    <?php foreach($data->gridImages()->toFiles() as $file): ?>
+      <div class="imgGrid__item">
 
-      <div class="imgGrid__image" style="background-image: url(<?= $file->crop(456, 264, 'center')->url() ?>)">
-        <img src="<?= $file->crop(456, 264, 'center')->url() ?>" alt="<?= $file->alt() ?>">
-      </div>
-
-      <? if($file->caption()->isNotEmpty()): ?>
-        <div class="imgGrid__caption">
-          <p><?= $file->caption() ?></p>
+        <div class="imgGrid__image" style="background-image: url(<?= $file->crop(456, 264, 'center')->url() ?>)">
+          <img src="<?= $file->crop(456, 264, 'center')->url() ?>" alt="<?= $file->alt() ?>">
         </div>
-      <?php endif; ?>
 
-    </div>
-  <?php endforeach; ?>
+        <? if($file->caption()->isNotEmpty()): ?>
+          <div class="imgGrid__caption">
+            <p><?= $file->caption() ?></p>
+          </div>
+        <?php endif; ?>
 
+      </div>
+    <?php endforeach; ?>
+
+  </div>
 </div>
 
 <!-- todo: srcset images on this page -->
