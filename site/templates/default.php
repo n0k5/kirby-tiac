61b0de7ab6f9c7941a06e6eb0188ca74d55fa150
n0k5 <hey@chrish.design>
2019-06-10 16:48:36 +0100

    Bring up to date

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,14 @@
 <?php snippet('header') ?>
 
 <main>
-  <?php snippet('pageTitle'); ?>
-  <?php snippet('builder') ?>
+  <div class="projects">
+    <?php foreach($page->children() as $project): ?>
+      <div class="project <? e($project->cover()->toFile()->orientation() == 'landscape', 'project__thumb--landscape', 'project__thumb--portrait'); ?>" style="background-image: url('<?= $project->cover()->toFile()->crop(456, 264, 'center')->url(); ?>')">
+        <!-- <?= $project->title() ?> -->
+        <!-- <? dump($project) ?> -->
+      </div>
+    <?php endforeach; ?>
+  </div>
 </main>
 
 <?php snippet('footer') ?>
